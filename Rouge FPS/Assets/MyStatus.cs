17a2b0023045f0878b718834ce8c72407f21d753
永井 <kd1219858@st.kobedenshi.ac.jp>
2019-08-05 22:01:53 +0900

    自機ステータス追加

---
+++ Rouge FPS/Assets/MyStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MyStatus : MonoBehaviour
+{
+
+    private int hp;
+    private GameObject equip;
+
+    public void SetHp(int hp)
+    {
+        this.hp = hp;
+    }
+
+    public int GetHp()
+    {
+        return hp;
+    }
+
+    public void SetEquip(GameObject weapon)
+    {
+        equip = weapon;
+    }
+
+    public GameObject GetEquip()
+    {
+        return equip;
+    }
+}
