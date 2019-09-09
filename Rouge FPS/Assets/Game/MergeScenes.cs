88564a67240b45270bb408bbf64d7d9aa19b7472
永井 <kd1219858@st.kobedenshi.ac.jp>
2019-09-09 12:44:06 +0900

    一部設定変更

--- Rouge FPS/Assets/Game/MergeScenes.cs
+++ Rouge FPS/Assets/Game/MergeScenes.cs
@@ -8,6 +8,10 @@ public class MergeScenes : MonoBehaviour
         SceneManager.LoadScene("PlayerScene", LoadSceneMode.Additive);
         SceneManager.LoadScene("MapScene", LoadSceneMode.Additive);
 
+        Scene scene = SceneManager.GetSceneByName("MapScene");
+        GameObject[] rootObjects = scene.GetRootGameObjects();
+
+
 
         SceneManager.LoadScene("EnemyScene", LoadSceneMode.Additive);
 
