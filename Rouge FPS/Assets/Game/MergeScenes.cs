6d2648855b91e3eddb059da6a5083c136be1a5c1
永井 <kd1219858@st.kobedenshi.ac.jp>
2019-09-09 15:10:06 +0900

    GameSceneで動作が重くなる問題を解消

--- Rouge FPS/Assets/Game/MergeScenes.cs
+++ Rouge FPS/Assets/Game/MergeScenes.cs
@@ -2,14 +2,14 @@
 using UnityEngine.SceneManagement;
 public class MergeScenes : MonoBehaviour
 {
+    private static bool CameraSeter = false;
+
+ 
 
     void Start()
     {
         SceneManager.LoadScene("PlayerScene", LoadSceneMode.Additive);
         SceneManager.LoadScene("MapScene", LoadSceneMode.Additive);
-
-        Scene scene = SceneManager.GetSceneByName("MapScene");
-        GameObject[] rootObjects = scene.GetRootGameObjects();
 
 
 
@@ -18,5 +18,24 @@ public class MergeScenes : MonoBehaviour
 
         SceneManager.LoadScene("SkillScene", LoadSceneMode.Additive);
         SceneManager.LoadScene("WeaponScene", LoadSceneMode.Additive);
+
+        //現在読み込まれているシーン数だけループ
+        for (int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
+        {
+            //読み込まれているシーンを取得し、その名前をログに表示
+            string sceneName = UnityEngine.SceneManagement.SceneManager.GetSceneAt(i).name;
+          
+            if (sceneName == "GameScene")
+            {
+                CameraSeter = true;
+                break;
+            }
+            
+        }
+    }
+    public static bool CameraSet
+    {
+        get { return CameraSeter; }  //取得用
+
     }
 }
