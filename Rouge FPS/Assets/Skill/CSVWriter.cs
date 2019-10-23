c9db71a92bba8c5976ee24a29b5b1e48b1fe74b9
pnng <kd1255087@st.kobedenshi.ac.jp>
2019-10-23 13:58:23 +0900

    いったんコミット

---
+++ Rouge FPS/Assets/Skill/CSVWriter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVWriter : MonoBehaviour
+{
+    Param param;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    // 引数はファイル名、上書き保存するかどうか(trueで上書き、falseで新規作成)
+    public void Write(string _pass, bool _b)
+    {
+        string s = "Assets/Resources/CSV/" + _pass;
+        try
+        {
+            // 出力用のファイルを開く
+            // 引数のtrueをfalseにするとファイル新規作成
+            using (var sw = new System.IO.StreamWriter(s, _b))
+            {
+                /*
+                for (int i = 0; i < x.Length; ++i)
+                {
+                    // 
+                    sw.WriteLine(, , y[i], z[i]);
+                }
+                */
+            }
+        }
+        catch (System.Exception e)
+        {
+            // ファイルを開くのに失敗したときエラーメッセージを表示
+            System.Console.WriteLine(e.Message);
+        }
+    }
+
+
+}
