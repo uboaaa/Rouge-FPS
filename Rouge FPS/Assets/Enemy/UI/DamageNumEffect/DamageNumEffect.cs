c97555bf78adf942e58ee4b077ba12b63660878a
玻名城 洋一 <kd1243183@st.kobedenshi.ac.jp>
2019-11-13 14:30:57 +0900

    なんかしてた

--- Rouge FPS/Assets/Enemy/UI/DamageNumEffect/DamageNumEffect.cs
+++ Rouge FPS/Assets/Enemy/UI/DamageNumEffect/DamageNumEffect.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class DamageNumEffect : MonoBehaviour
 {
+
     GameObject enemy;
     EnemyHitEffect eh;
-    GameObject DamageNum;
+    
     public bool hitflg = false;
     public int BulletDamage = 0;
-    private bool FeedOutFlg = false;
-    private float AlphaTime = 1.0f;
 
+    GameObject DamageNumPrefab;
+    // private struct DamageNumParam
+    // {
+        public bool FeedOutFlg = false; 
+        public float AlphaTime;
+    //}
+    
 
+    // DamageNumParam dnp;
+    // List<DamageNumParam> dnpList;
 
-        
+    // private List<float> Alpha;  
 
 
 
@@ -26,7 +34,9 @@ public class DamageNumEffect : MonoBehaviour
         // エネミーヒットエフェクト
         eh = GetComponent<EnemyHitEffect>();
 
-        DamageNum = Resources.Load("DamageNum") as GameObject;
+        DamageNumPrefab = Resources.Load("DamageNum") as GameObject;
+
+        //DamageNumParam[] dnp = new DamageNumParam[10];
     }
 
     GameObject dn;
@@ -36,38 +46,58 @@ public class DamageNumEffect : MonoBehaviour
         if(hitflg == true)
         {
             // ダメージの値を挿入
-            DamageNum.GetComponent<TextMesh>().text = "" + BulletDamage;
+            DamageNumPrefab.GetComponent<TextMesh>().text = "" + BulletDamage;
 
             // プレハブ生成
-            dn = Instantiate(DamageNum) as GameObject;
+            dn = Instantiate(DamageNumPrefab) as GameObject;
+            
 
             // 敵の座標へ移動
             float value = Random.Range(-1.0f, 1.0f);    // 乱数
             dn.gameObject.transform.position = new Vector3(enemy.gameObject.transform.position.x + value,enemy.gameObject.transform.position.y,enemy.gameObject.transform.position.z + value);
 
             // 向き方向の取得
-            Vector3 force = new Vector3(0.0f,1.0f,0);
+            Vector3 force = new Vector3(0.0f,3.5f,0);
 
             // Rigidbodyに力を加えて発射
             dn.GetComponent<Rigidbody>().AddForce(force,ForceMode.Impulse);
 
 
-            Destroy(dn, 2.0f); // 2秒後に削除
-
+            Destroy(dn, 0.6f); // 2秒後に削除
+            //Alpha.Add(1.0f);
+            // dnpList.Add();
             FeedOutFlg = true;
+
             AlphaTime = 1.0f;
+            //TextMesh tm = dn.GetComponent<TextMesh>();
+            //tm.color = new Color(1.0f,1.0f,1.0f,1.0f);
+
             hitflg = false;
         }
 
+        
         if(FeedOutFlg)
         {
-            if(this.gameObject)
+            if(dn)
             {
-            TextMesh tm = dn.GetComponent<TextMesh>();
-            
-            tm.color = new Color(1.0f,1.0f,1.0f,AlphaTime);
+                TextMesh tm = dn.GetComponent<TextMesh>();
+                tm.color = new Color(1.0f,1.0f,1.0f,AlphaTime);
+                
+                // if(tm.color.a > 0.0f)
+                // {
+                //     float AlphaTime = tm.color.a;
+                //     AlphaTime -= 0.05f;
+                //     tm.color = new Color(1.0f,1.0f,1.0f,tm.color.a);
+                // }
+                // else
+                // {
+                //     AlphaTime = 1.0f;
+                //     FeedOutFlg = false;
+                //     Destroy(dn);
+                // }
             }
-            AlphaTime -= 0.01f;
+            AlphaTime -= 0.05f;
+            
             if(AlphaTime < 0.0f)
             {
                 AlphaTime = 0.0f;
--- Rouge FPS/Assets/Enemy/UI/ItemBox/FlyingObject.cs
+++ Rouge FPS/Assets/Enemy/UI/ItemBox/FlyingObject.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class FlyingObject : MonoBehaviour
 {
     public float FlyPow;
+    public float posisionY;
     
     // Sin波を使い上下に揺らす
     void Update () {
         transform.position = new Vector3(transform.position.x
-        , 0.5f + Mathf.Sin (Time.frameCount * FlyPow ) / 2,transform.position.z);
+        , posisionY + Mathf.Sin (Time.frameCount * FlyPow ) / 2,transform.position.z);
     }
 }
