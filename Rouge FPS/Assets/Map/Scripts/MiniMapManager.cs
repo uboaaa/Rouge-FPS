3c6d16b40153c1d4ea1971f7b8ebdaf03bd0b6ac
玉岡真 <kd1253011@st.kobedenshi.ac.jp>
2019-11-26 13:28:09 +0900

    リベース競合の解決

--- Rouge FPS/Assets/Map/Scripts/MiniMapManager.cs
+++ Rouge FPS/Assets/Map/Scripts/MiniMapManager.cs
@@ -24,11 +24,6 @@ public class MiniMapManager : MonoBehaviour
         
     }
 
-    private void LateUpdate()
-    {
-        
-    }
-
     //マップチップ生成
     public void CreateMapTip(int[,] mapdata)
     {
