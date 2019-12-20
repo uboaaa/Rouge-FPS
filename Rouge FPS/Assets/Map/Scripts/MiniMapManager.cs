9fd5e78a7f7bdb81e343e52e8f1ca8af6985d81e
玉岡真 <kd1253011@st.kobedenshi.ac.jp>
2019-12-20 09:28:03 +0900

    ミニマップが主人公中心になるように変更、部屋に入ると同時に一気に表示

--- Rouge FPS/Assets/Map/Scripts/MiniMapManager.cs
+++ Rouge FPS/Assets/Map/Scripts/MiniMapManager.cs
@@ -14,11 +14,12 @@ public class MiniMapManager : MonoBehaviour
     //ミニマップ用オブジェクト
     [SerializeField]
     private GameObject m_miniMap = null;
-
-    //マーカー用オブジェクト
     [SerializeField]
-    private GameObject m_playerMarker = null;
-    private RectTransform m_markRect = null;
+    private GameObject m_wholeMap = null;
+    [SerializeField]
+    private GameObject m_raderCamera = null;
+    [SerializeField]
+    private GameObject m_raderPlayer = null;
 
     //確認用プレイヤーオブジェクト
     [SerializeField]
@@ -30,99 +31,51 @@ public class MiniMapManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_markRect = m_playerMarker.gameObject.GetComponent<RectTransform>();
+        m_wholeMap.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //レーダ表示マップ
+        float x = 0, z = 0;
+        Vector3 euler = new Vector3();
+        if (MergeScenes.IsMerge())
+        {
+            x = PlayerXYZ.GetPlayerPosition("px");
+            z = PlayerXYZ.GetPlayerPosition("pz");
+            euler = PlayerXYZ.GetPlayerRotation();
+        }
+        else
+        {
+            x = m_target.transform.position.x;
+            z = m_target.transform.position.z;
+            euler = m_target.transform.localEulerAngles;
+        }
+        m_raderPlayer.transform.position = new Vector3(x, 10, z);               //プレイヤーの座標を更新
+        m_raderPlayer.transform.rotation = Quaternion.Euler(90, 0, -euler.y);   //プレイヤーの回転を更新
+        m_raderCamera.transform.position = new Vector3(x, 200, z);              //プレイヤーの真上にカメラをセット
+        m_raderCamera.transform.rotation = Quaternion.Euler(90, euler.y, 0);    //カメラを真下に向ける
+
+        //TabキーでマップON/OFF
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (m_miniMap.activeSelf)
             {
                 m_miniMap.SetActive(false);
-                m_playerMarker.SetActive(false);
+                m_wholeMap.SetActive(true);
+
             }
             else
             {
                 m_miniMap.SetActive(true);
-                m_playerMarker.SetActive(true);
+                m_wholeMap.SetActive(false);
             }
         } 
     }
 
     private void LateUpdate()
     {
-        //プレイヤーの配置データを取得
-        //右上がアンカーなのでマップサイズ分を引いて補正
-        float x = 0, y = 0;
-        Vector3 euler = new Vector3();
-        if (MergeScenes.IsMerge())
-        {
-            x = PlayerXYZ.GetPlayerPosition("px") / MapInitializer.MAP_SCALE ;
-            y = PlayerXYZ.GetPlayerPosition("pz") / MapInitializer.MAP_SCALE ;
-            euler = PlayerXYZ.GetPlayerRotation();
-        }
-        else
-        {
-            x = (m_target.transform.position.x) / MapInitializer.MAP_SCALE ;
-            y = (m_target.transform.position.z) / MapInitializer.MAP_SCALE ;
-            euler = m_target.transform.localEulerAngles;
-        }
 
-        //プレイヤーの周囲のチップをONにする
-        int tipx = (int)x + 1;
-        int tipy = (int)y + 1;
-        for(int i = -2; i < 3; i++)
-        {
-            for(int j = -2; j < 3; j++)
-            {
-                //範囲外の要素数は飛ばす
-                if (tipx + i < 0 || tipx + i >= MapInitializer.MAP_SIZE_X || tipy < 0 || tipy >= MapInitializer.MAP_SIZE_Y) continue;
-                //コンポーネントがある時、Enableをtrueにする
-                if (m_tipArray[tipx + i, tipy + j] != null)
-                {   
-                    m_tipArray[tipx + i, tipy + j].m_tipEnable.Value = true;
-                }
-            }
-        }
-        
-        //マーカーのサイズ分を乗算
-        float posX = (x - MapInitializer.MAP_SIZE_X / 2 + 1) * 5;
-        float posY = (y - MapInitializer.MAP_SIZE_Y / 2 + 1) * 5;
-
-        //アンカーを基準に座標、回転を設定
-        m_markRect.anchoredPosition = new Vector3(posX - m_markRect.sizeDelta.x / 2.0f, posY - m_markRect.sizeDelta.y / 2.0f);          //m_markRect.sizeDeleta.x/2.0fはズレ補正分
-        m_markRect.rotation = Quaternion.Euler(0, 0, -euler.y);
     }
-
-    //マップチップ生成
-    public void CreateMapTip(int[,] map)
-    {
-        //マップをセット
-        mapdata = map;
-
-        //チップ配列を初期化
-        m_tipArray = new MapTip[MapInitializer.MAP_SIZE_X, MapInitializer.MAP_SIZE_Y];
-
-        for(int y = 0; y < MapInitializer.MAP_SIZE_Y; y++)
-        {
-            for(int x = 0; x < MapInitializer.MAP_SIZE_X; x++)
-            {
-                if (mapdata[x, y] == 0)
-                {
-                    //マップチップのオブジェクト生成
-                    GameObject mapTip = Instantiate(m_tipPrefab) as GameObject;
-
-                    //親にミニマップ用オブジェクトを設定
-                    mapTip.transform.SetParent(m_miniMap.transform);
-
-                    //マップチップの配置情報を初期化、登録
-                    m_tipArray[x, y] = mapTip.GetComponent<MapTip>();
-                    m_tipArray[x, y].Initialize(x, y, MapInitializer.MAP_SIZE_X / 2, MapInitializer.MAP_SIZE_Y / 2);
-                }
-            }
-        }
-    }
-
 }
---
+++ Rouge FPS/Assets/Map/Scripts/RoomEnabler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomEnabler : MonoBehaviour
+{
+    public BoxCollider collider = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            collider.enabled = true;
+        }
+    }
+}
