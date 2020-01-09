f02b594816b07523e7d9a2c7efd8c453af63355e
永井 <kd1219858@st.kobedenshi.ac.jp>
2020-01-09 10:19:03 +0900

    横転追加

---
+++ Rouge FPS/Assets/Player/Script/Player/PlayerDown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDown : MonoBehaviour
+{
+
+    
+  GameObject FPSCon;
+  Transform myTransform2;
+  float z;
+    // Start is called before the first frame update
+    void Start()
+    {
+    FPSCon = GameObject.Find("FPSController");
+    myTransform2 = this.transform;
+    z=0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ // ワールド座標を基準に、回転を取得
+        Vector3 worldAngle = myTransform2.eulerAngles;
+    if(FPSCon.GetComponent<MyStatus>().GetHp()<1){
+        if(worldAngle.z<91.0f){
+        worldAngle.z +=1.0f;  // ワールド座標を基準に、z軸を軸にした回転を10度に変更
+        myTransform2.eulerAngles = worldAngle; // 回転角度を設定
+        }
+    }
+
+    }
+}
