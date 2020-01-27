bda21ae8bc588ee072fdbc62188c523231be4982
玻名城 洋一 <kd1243183@st.kobedenshi.ac.jp>
2020-01-27 13:37:00 +0900

    敵にランダム生成用枠を追加

--- Rouge FPS/Assets/Enemy/Lizard/Lizard.cs
+++ Rouge FPS/Assets/Enemy/Lizard/Lizard.cs
@@ -44,7 +44,10 @@ public class Lizard : MonoBehaviour
     int trans = 0;
 
     // 死んだ時に生成されるオブジェクト
-    public GameObject PopObject = null;
+    private int rnd = 0;
+    public GameObject PopObject1 = null;
+
+    public GameObject PopObject2 = null;
 
 
 
@@ -250,12 +253,34 @@ public class Lizard : MonoBehaviour
             de.transform.position = this.gameObject.transform.position;
             de.transform.position = new Vector3(de.transform.position.x,de.transform.position.y - 1.2f,de.transform.position.z);
 
+            // 乱数処理
+            rnd = Random.Range(0, 3);
+
             // 死んだときにアイテムポップ
-            if(PopObject)
+            // 0は無生成
+            if(rnd == 0)
             {
-                GameObject po = Instantiate(PopObject) as GameObject;
-                po.transform.position = this.gameObject.transform.position;
+
             }
+            // 1を生成
+            else if(rnd == 1)
+            {
+                if(PopObject1)
+                {
+                    GameObject po1 = Instantiate(PopObject1) as GameObject;
+                    po1.transform.position = this.gameObject.transform.position;
+                }
+            }
+            // 2を生成
+            else if(rnd == 2)
+            {
+                if(PopObject2)
+                {
+                    GameObject po2= Instantiate(PopObject2) as GameObject;
+                    po2.transform.position = this.gameObject.transform.position;
+                }
+            }
+            
 
             // 解放処理
             Destroy(de,2.0f);
