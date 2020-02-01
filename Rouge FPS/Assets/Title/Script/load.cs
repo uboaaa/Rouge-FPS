4109fdcfbd2f042536f34806e31f2edaa0f84316
永井 <kd1219858@st.kobedenshi.ac.jp>
2020-02-01 14:47:52 +0900

    ロード画面追加

---
+++ Rouge FPS/Assets/Title/Script/load.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class load : MonoBehaviour
+{
+    [SerializeField] private Slider _loadingBar;
+    private AsyncOperation _async;
+
+
+  
+	IEnumerator Start () {
+        yield return new WaitForSeconds(1);
+
+  
+
+        //非同期でロード開始
+        _async = SceneManager.LoadSceneAsync("GameScene");
+        
+ 
+        //シーン移動を許可するかどうか
+        _async.allowSceneActivation = false;
+
+        while (_async.progress < 0.9f) //0.9で止まる
+        {
+             Debug.Log(_async.progress);
+            _loadingBar.value = _async.progress;
+            yield return 0;
+        }
+        Debug.Log("ロード完了");
+        _loadingBar.value = 1.0f;
+        _async.allowSceneActivation = true;
+
+        yield return _async;
+    }
+
+    
+}
